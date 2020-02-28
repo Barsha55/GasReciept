c0dcb6a4a48cd83a9770793f97ab9b8e4168379f
barsha <barsha.sangraula.2022@students.lhschools.org>
2020-02-28 12:03:13 -0500

    Intellij commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("+--------------------------------------------------+");
+        System.out.println("|    Sangraula's Gas Station                       |");
+        System.out.println("|   274 Stonemast loop                             |");
+        System.out.println("|   Blacklick, Ohio 43062                          |");
+        System.out.println("|   Date                     Time                  |");
+        System.out.println("|   01/16/20                 4:00 PM               |");
+        System.out.println("|   Invoice Number#          45876                 |");
+        System.out.println("|   Auth#                                          |");
+        System.out.println("|   Mastercard                                     |");
+        System.out.println("|   ***********23                                  |");
+        System.out.println("|   pump                    Product        $G      |");
+        System.out.println("|    1                      unleaded       2.50    |");
+        System.out.println("|  Amount                   Fuel Total             |");
+        System.out.println("|  10.00                    $25.50                 |");
+        System.out.println("|              Thank you!                          |");
+        System.out.println("|              Sangraula's Gas Station             |");
+        System.out.println("+--------------------------------------------------+");
+    }
+}
